a31b11b7cc855d33e75d958ebbdc2ae9f73000ff
Maaxboon <maaxwelokoth@gmail.com>
2024-11-29 17:26:01 +0300

    feat: build initial AccommodationListings view with placeholder components and layout

--- src/views/AccommodationListings.tsx
+++ src/views/AccommodationListings.tsx
@@ -1,3 +1,17 @@
 export default function AccommodationListings() {
   return <p>WELCOME FROM AccommodationListings</p>;
 }
+
+const Header = () => <>IM HEADER</>;
+const Search = () => <>IM SEARCH</>;
+const Listings = () => <>IM LISTINGS</>;
+
+export default function AccommodationListings() {
+  return (
+    <>
+      <Header />
+      <Search />
+      <Listings />
+    </>
+  );
+}
